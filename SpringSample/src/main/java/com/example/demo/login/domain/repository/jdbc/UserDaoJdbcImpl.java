ba4feb5bb12d97de5ebd2d1e1dc6213d5fbd6264
Masashi <t15519ms@gmail.com>
2020-08-29 13:07:36 +0900

    add: insert(sign up)機能

--- SpringSample/src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
+++ SpringSample/src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
@@ -23,7 +23,13 @@ public class UserDaoJdbcImpl implements UserDao{
 
 	@Override
 	public int insertOne(User user) throws DataAccessException {
-		return 0;
+		//1件登録
+		int rowNumber = jdbc.update("INSERT INTO m_user(user_id,"
+				+ " password," + " user_name," + " birthday," +
+				" age," + " marriage," + " role)" + " VALUES(?,?,?,?,?,?,?)"
+				, user.getUserId(), user.getPassword(), user.getUserName(),
+				 user.getBirthday(), user.getAge(), user.isMarriage(), user.getRole());
+		return rowNumber;
 	}
 
 	@Override
