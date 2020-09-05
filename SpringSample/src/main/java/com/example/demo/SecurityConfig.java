0c430e9522fa031ae3407e1d45b8b63a641c0ad7
Masashi <t15519ms@gmail.com>
2020-09-05 16:30:26 +0900

    add: Rest環境設定とselectの実装

--- SpringSample/src/main/java/com/example/demo/SecurityConfig.java
+++ SpringSample/src/main/java/com/example/demo/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/css/**").permitAll()
 			.antMatchers("/login").permitAll()
 			.antMatchers("/signup").permitAll()
+			.antMatchers("/rest/**").permitAll()
 			.antMatchers("/admin").hasAuthority("ROLE_ADMIN")
 			.anyRequest().authenticated();
 
@@ -65,8 +67,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.logoutUrl("/logout")
 			.logoutSuccessUrl("/login");
 
-		//CSRF対策を無効に設定
-		//http.csrf().disable();
+		//CSRFを無効にするURLを設定
+		RequestMatcher csrfMatcher = new RestMatcher("/rest/**");
+		//RESTのみCSRF対策を無効に設定
+		http.csrf().requireCsrfProtectionMatcher(csrfMatcher);
 	}
 
 	@Override
